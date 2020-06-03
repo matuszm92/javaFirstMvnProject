0ae7ced7eab9e037fd73bf5adfa43bdbe99c8ab7
matuszm92 <marcel.matuszak@gmail.com>
2020-06-03 17:23:40 +0200

    Clean up and create status endpoint with presentation on main page

--- javaFirstProject/src/main/java/com/zaprogramowani/App.java
+++ javaFirstProject/src/main/java/com/zaprogramowani/App.java
@@ -2,60 +2,15 @@ package com.zaprogramowani;
 
 import static spark.Spark.*;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
 public class App {
 
-    static List<User> registeredUsers;
-
     public static void main(String[] args) {
-        registeredUsers = new ArrayList<>();
-
-        registeredUsers.add(new User("janusz", "paciaciak"));
-        registeredUsers.add(new User("admin", "admin"));
-
         staticFiles.location("/public");
-        
-        get("/hello/:name", (req, res) -> {
-            return "Witaj: " + req.params(":name");
+
+        get("/status", (req, res) -> {
+            res.type("application/json");
+            return "{\"message\":\"OK\"}";
         });
-
-        post("/getDate", (req,res) -> {
-            return LocalDate.now(); 
-        });
-     
-        get("/login/:login/:pass", (req,res) -> {
-            String login = req.params(":login");
-            String password = req.params(":pass");
-            
-            for(User user : registeredUsers) {
-                if(user.getLogin().equals(login) && user.getPassword().equals(password)) {
-                    return "Jestes zalogowany";
-                }
-            }
-            return "Cos poszlo nie tak";
-        });
-
-    }
-
-    static class User {
-        private String login;
-        private String password;
-
-        public User(String login, String password) {
-            this.login = login;
-            this.password = password;
-        }
-
-        public String getLogin() {
-            return login;
-        }
-
-        public String getPassword() {
-            return password;
-        }
 
     }
 }
